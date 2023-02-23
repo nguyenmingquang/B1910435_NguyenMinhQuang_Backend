95a0e754a3a1de99883462a434f8aa0a68369121
nguyenmingquang <quangB1910435@student.ctu.edu.vn>
2023-02-23 11:49:24 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
-    return res.status(Error.statusCode || 500).json({
-    message: Error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
     });
 });
-    
+
 module.exports = app;
